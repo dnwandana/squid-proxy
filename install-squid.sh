eb453005d48a7f733240ba5558085261fa1a06d0
Wandana <dn.wandana@gmail.com>
2021-02-16 21:38:19 +0700

    Script improvement

--- install-squid.sh
+++ install-squid.sh
@@ -17,11 +17,13 @@ sudo touch /etc/squid/passwd
 # change the ownership to proxy user.
 sudo chown proxy /etc/squid/passwd
 
+# clear screen
+clear
+
 # create user for proxy service.
 read -p "Create new user: " PROXY_USER
-if [[ $PROXY_USER != "" ]]
-then
-    sudo htpasswd /etc/squid/passwd $PROXY_USER
+if [ "$PROXY_USER" != "" ]; then
+    sudo htpasswd /etc/squid/passwd "$PROXY_USER"
 fi
 
 # replace old squid.conf with new conf. file.
@@ -30,14 +32,16 @@ sudo cp squid.conf /etc/squid
 # restart squid service.
 sudo systemctl restart squid
 
+# clear screen
+clear
+
 # done.
-if [[ $PROXY_USER != "" ]]
-then
-    echo "Installation Done!"
-    echo "now, you can use proxy with user that you've created before."
+if [ "$PROXY_USER" != "" ]; then
+    echo -e "\n\nInstallation Done!"
+    echo -e "Now, you can use proxy with a user that you've created before.\n\n"
 else
-    echo "Installation Done,"
-    echo "But You're skipping user proxy creation."
-    echo -e "type 'sudo htpasswd /etc/squid/passwd \e[92musername\e[0m'."
-    echo "and then you're ready to go."
+    echo -e "\n\nInstallation Done,"
+    echo "But you're skipping user proxy creation."
+    echo -e "To add a proxy user, type 'sudo htpasswd /etc/squid/passwd \e[92musername\e[0m'."
+    echo -e "And then you're ready to go.\n\n"
 fi
